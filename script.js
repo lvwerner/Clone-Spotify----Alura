a15473b4fa802b12cba1c8b223e30708a2e8b8ba
lvwerner <viniciushhardt@gmail.com>
2024-01-25 19:12:45 -0300

    update aula 4

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 const searchInput = document.getElementById('search-input');
-const resultArtists = document.getElementById('result-artist');
-const resultPlaylists = document.getElementById('result-playlists');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
 
-
-function requestApi(searchTerm){
-    const url = "http://localhost:3000/artists?name_like=${searchTerm}";
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
     fetch(url)
-    .then((response) => response.json())
-    .then((result) => displayResults(result))
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
 }
 
-function displayResults(result){
-    resultPlaylists.classList.add('hidden');
-    const artistsName = document.getElementById('artists-name');
-    const artistsImage = document.getElementById('artists-image');
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
 
     result.forEach(element => {
-        artistsName.innerText = element.name;
-        artistsImage.src = element.urlImage;
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
     });
 
-    resultArtists.classList.remove('hidden');
+    resultArtist.classList.remove('hidden');
 }
 
-//  document.addEventListener('input', function(){
-//     const searchTerm = searchInput.value.toLowerCase();
-//     if(searchTerm ===''){
-//         resultPlaylists.classList.add('hidden');
-//         resultArtists.classList.remove('hidden');
-//         return;
-//     }
-
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
     
-//  })
+    requestApi(searchTerm);
+})
